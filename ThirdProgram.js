9b4ae38b7f1aa888ffe1327efc32f832c675bf03
Srishti Singh <srsingh@axway.com>
2022-09-15 06:16:54 +0530

    added

--- ThirdProgram.js
+++ ThirdProgram.js
@@ -1,4 +1,4 @@
 var a = 100; // TODO Update to 250 ;
-var b = 200;
+var b = 250;
 console.log(a + b);
 
---
+++ program.js
@@ -0,0 +1,12 @@
+var a = 100
+var b = 200
+console.log(a+b)
+
+for (let i = 0; i < 10; i++) {
+    console.log(i + " ");
+    
+
+}
+if(a > b )
+console.log('a is greater than b')
+console.log("Hii");
